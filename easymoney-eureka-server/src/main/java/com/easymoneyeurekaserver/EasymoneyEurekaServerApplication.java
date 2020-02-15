027abf3df2c99ef5540bfcd0954a8fb754d90a21
urtiaga <urtiaga@urtiagas-MacBook-Pro.local>
2020-02-14 22:10:26 -0300

    inclui config server e eureka server

---
+++ easymoney-eureka-server/src/main/java/com/easymoneyeurekaserver/EasymoneyEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.easymoneyeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymoneyEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymoneyEurekaServerApplication.class, args);
+	}
+
+}
