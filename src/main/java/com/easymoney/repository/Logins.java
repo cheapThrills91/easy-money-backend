078fd47fc79c64d4e8257edca192f80e1aee4976
Rafael Santiago <rafaelmsantiago@gmail.com>
2020-03-08 23:22:01 -0300

    Criação do Endpoint Login

---
+++ src/main/java/com/easymoney/repository/Logins.java
@@ -0,0 +1,9 @@
+package com.easymoney.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.easymoney.model.Login;
+
+public interface Logins extends JpaRepository<Login, Long> {
+
+}
---
+++ src/main/java/com/easymoney/resource/LoginResource.java
@@ -0,0 +1,78 @@
+package com.easymoney.resource;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.easymoney.model.Login;
+import com.easymoney.repository.Logins;
+
+@RestController
+@RequestMapping("/logins")
+public class LoginResource {
+	
+	@Autowired
+	private Logins logins;
+	
+	@PostMapping
+	public Login adicionar(@Valid @RequestBody Login login) {
+		return logins.save(login);
+	}
+	
+	@GetMapping
+	public List<Login> listar() {
+		return logins.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Login> buscar(@PathVariable Long id) {
+		Login login = logins.getOne(id);
+		
+		if (login == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(login);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Login> atualizar(@PathVariable Long id, 
+			@Valid @RequestBody Login login) {
+		Login existente = logins.getOne(id);
+		
+		if (existente == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		BeanUtils.copyProperties(login, existente, "id");
+		
+		existente = logins.save(existente);
+		
+		return ResponseEntity.ok(existente);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		Login login = logins.getOne(id);
+		
+		if (login == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		logins.delete(login);
+		
+		return ResponseEntity.noContent().build();
+	}
+}
